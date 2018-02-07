3749cfb68e7bb95f0769160f33a0b3b35acfe493
Silviu <silviu.curuciuc@gmail.com>
2018-02-07 14:09:47 +0200

    commit

--- src/main/java/com/fortech/productservice/ProductServiceApplication.java
+++ src/main/java/com/fortech/productservice/ProductServiceApplication.java
@@ -2,11 +2,17 @@ package com.fortech.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class ProductServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
+
+    @GetMapping
+    public String hello() {
+        return "hello";
+    }
 }
