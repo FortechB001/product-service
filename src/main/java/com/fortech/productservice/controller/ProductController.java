3c189584332e1f7989d5643423d6e0885b02643d
Marius <aniov_usa@yahoo.com>
2018-02-08 15:48:30 +0200

    add cross origin

--- src/main/java/com/fortech/productservice/controller/ProductController.java
+++ src/main/java/com/fortech/productservice/controller/ProductController.java
@@ -10,6 +10,7 @@ import java.util.List;
 /**
  * Rest Controller for the Product related requests.
  */
+@CrossOrigin(origins = "http://localhost:8100")
 @RestController
 @RequestMapping("/product")
 public class ProductController {
