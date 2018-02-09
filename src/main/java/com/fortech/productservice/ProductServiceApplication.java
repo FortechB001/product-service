c77373e864fcf4e8cad9a62677ccd1490b2b1088
Silviu <silviu.curuciuc@gmail.com>
2018-02-09 14:05:50 +0200

    Enable Eureka client

--- src/main/java/com/fortech/productservice/ProductServiceApplication.java
+++ src/main/java/com/fortech/productservice/ProductServiceApplication.java
@@ -2,8 +2,10 @@ package com.fortech.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ProductServiceApplication {
 
@@ -11,8 +13,4 @@ public class ProductServiceApplication {
         SpringApplication.run(ProductServiceApplication.class, args);
     }
 
-    @GetMapping
-    public String hello() {
-        return "hello";
-    }
 }
