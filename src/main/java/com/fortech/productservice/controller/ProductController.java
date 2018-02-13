017b71943d620a6d22d6a22c68340ae777ce3831
Marius <aniov_usa@yahoo.com>
2018-02-13 17:44:29 +0200

    Added methods to work with admin page

--- src/main/java/com/fortech/productservice/controller/ProductController.java
+++ src/main/java/com/fortech/productservice/controller/ProductController.java
@@ -2,8 +2,14 @@ package com.fortech.productservice.controller;
 
 import com.fortech.productservice.model.Product;
 import com.fortech.productservice.repository.ProductRepository;
+import com.fortech.productservice.util.Message;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -21,6 +27,9 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     /**
      * Displays all products.
      *
@@ -37,9 +46,62 @@ public class ProductController {
      * @param name        - the name.
      * @param description - the description.
      */
-    @PostMapping("/add")
-    public void addStock(@RequestParam(name = "name") String name,
-                         @RequestParam(name = "description") String description) {
-        productRepository.save(new Product(name, description));
+    @PostMapping(value = "/add")
+    public ResponseEntity<?> addStock(@RequestParam(name = "name") String name,
+                                      @RequestParam(name = "description", required = false, defaultValue = "Fruit's are awesome") String description) {
+
+        if (productRepository.findByNameIgnoreCase(name).isEmpty()) {
+            productRepository.save(new Product(name, description));
+            return ResponseEntity.ok().build();
+        } else {
+            return new ResponseEntity<>(new Message("Product already exists with name:" + name), HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
+
+    /**
+     * Update Product Name
+     *
+     * @param productId      - product Id
+     * @param newProductName - new product name
+     */
+    @PutMapping(path = "/productName/update")
+    public ResponseEntity<?> changeProductName(@RequestParam(name = "productId") String productId,
+                                               @RequestParam(name = "newProductName") String newProductName) {
+
+        Product product = productRepository.findOne(productId);
+        if (productRepository.findByNameIgnoreCase(newProductName).isEmpty()) {
+            product.setName(newProductName);
+            productRepository.save(product);
+            return ResponseEntity.ok().build();
+        }
+        return new ResponseEntity<>(new Message("Product already exists with name:" + newProductName), HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    /**
+     * Delete a product and all stock related
+     *
+     * @param productId - product Id
+     * @return
+     */
+    @DeleteMapping(path = "/delete")
+    public ResponseEntity<?> deleteProduct(@RequestParam(name = "productId") String productId) {
+
+        Product productToBeDeleted = productRepository.findOne(productId);
+        if (productToBeDeleted == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        try {
+            String url = "http://stock-service/stock/delete?productId=" + productId;
+            ResponseEntity<?> responseEntity = restTemplate.exchange(url, HttpMethod.DELETE, null,
+                    new ParameterizedTypeReference<ResponseEntity>() {
+                    });
+            productRepository.delete(productId);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+
+
     }
 }
---
+++ src/main/java/com/fortech/productservice/util/Message.java
@@ -0,0 +1,12 @@
+package com.fortech.productservice.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+public class Message implements Serializable {
+    private String message;
+}
